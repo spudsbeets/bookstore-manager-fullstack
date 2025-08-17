217479796491fd0fb7f29da9d7bbeb82c0135cdd
Sean Miller <millersean757@gmail.com>
2025-08-17 13:42:18 -0600

    sanitized db-connector

--- controllerAndModel/database/db-connector.js
+++ controllerAndModel/database/db-connector.js
@@ -17,10 +17,10 @@ const pool = mysql
    .createPool({
       waitForConnections: true,
       connectionLimit: 10,
-      host: "classmysql.engr.oregonstate.edu",
-      user: "cs340_bradsean",
-      password: "9823",
-      database: "cs340_bradsean",
+      host: "",
+      user: "",
+      password: "",
+      database: "",
       connectTimeout: 60000,
    })
    .promise(); // This makes it so we can use async / await rather than callbacks
